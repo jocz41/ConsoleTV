e8f0adb19be119838378b1abc0975cd3ae1870b5
Sara <mublasa@gmail.com>
2019-11-13 19:22:00 +0100

    First commit

---
+++ ConsoleApp1/Principal.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica1
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Tv tele = new Tv();
+            Principal p = new Principal();
+            ConsoleKey[] teclas = { ConsoleKey.Enter, ConsoleKey.Spacebar, ConsoleKey.Backspace, ConsoleKey.UpArrow,
+                                    ConsoleKey.DownArrow, ConsoleKey.I, ConsoleKey.Escape};
+
+            ConsoleKey ck;
+
+            p.datosTV(ref tele);
+
+            p.menu();
+            ck = Console.ReadKey().Key;
+
+            do
+            {
+                p.seleccion(ck, tele);
+                int n;
+                
+                do
+                {
+                    System.Threading.Thread.Sleep(1);
+                    ck = Console.ReadKey().Key;
+                    n = Array.IndexOf(teclas, ck);
+                    Console.WriteLine(n);
+                } while (n < 0);
+
+                p.menu();
+                                
+            } while (ck != ConsoleKey.Escape);
+            
+        }
+
+        #region Imprimir especiales
+        public static void impVerde(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+
+        public static void impCyan(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+        
+        public static void impError(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+
+        #endregion
+
+        #region Comprobación de Inputs
+        private int comprobarNumInt(string s)
+        {
+            int n;
+            bool esNum = int.TryParse(s, out n);
+
+            if (s.Length == 0)  //Comprueba que no esté vacío
+            {
+                impError("\nERROR. No ha introducido ningún valor.\n");
+            }
+            else if (!esNum)    //Comprueba que sea un valor numérico
+            {
+                impError("\nERROR. Valor no entero numérico.\n");
+            }
+            else if (s.Length > 8)  //Limita la longitud
+            {
+                impError("\nERROR. Número demasiado extenso.\n");
+                n = -1;
+            }
+
+            return n;
+        }
+
+        //Método para comprobar que la entrada sea un double
+        private double comprobarNumDouble(string s)
+        {
+            double n;
+
+            //Sustitución del punto por una coma en caso de decimal
+            s = s.Replace('.', ',');
+
+            bool esNum = double.TryParse(s, out n);
+
+            if (s.Length == 0)  //Comprueba que no esté vacío
+            {
+                impError("\nERROR. No ha introducido ningún valor.\n");
+            }
+            else if (!esNum)    //Comprueba que sea un valor numérico
+            {
+                impError("\nERROR. Valor no numérico.\n");
+            }
+            else if (s.Length > 15)  //Limita la longitud
+            {
+                impError("\nERROR. Número demasiado extenso.\n");
+                n = 0;
+            }
+
+            return n;
+        }
+
+        //Método pulsar para continuar
+        public void continuar()
+        {
+            Console.Write("\nPulse cualquier tecla para continuar.");
+            Console.ReadKey();
+            Console.WriteLine("\n");
+        }
+        #endregion
+
+        public void datosTV(ref Tv tele)
+        {
+            Console.WriteLine("Introduzca los datos de su nueva televisión:\n");
+
+            impVerde("Marca: ");
+            tele.Marca = Console.ReadLine();
+            
+            do
+            {
+                impVerde("\nPulgadas: ");
+                tele.Pulgadas = comprobarNumDouble(Console.ReadLine());
+            } while (tele.Pulgadas == 0);
+
+            do
+            {
+                impVerde("\nConsumo: ");
+                tele.Consumo = comprobarNumDouble(Console.ReadLine());
+            } while (tele.Consumo == 0);
+
+            do
+            {
+                impVerde("\nPrecio: ");
+                tele.Precio = comprobarNumDouble(Console.ReadLine());
+            } while (tele.Precio == 0);
+        }
+                
+        #region Menú
+        public void menu()
+        {
+            Console.Clear();
+            Console.WriteLine("┌──────────────────────────────────────────┐");
+            Console.WriteLine("|    Seleccione una opción:                |");
+            Console.WriteLine("| Enter.    Apagar/Encender                |");
+            Console.WriteLine("| Espacio.  Cambiar de canal               |");
+            Console.WriteLine("| Back.     Canal anterior                 |");
+            Console.WriteLine("| ↑.        Subir Volumen                  |");
+            Console.WriteLine("| ↓.        Bajar Volumen                  |");
+            Console.WriteLine("| i.        Información Técnica            |");
+            Console.WriteLine("| Esc.      Salir                          |");
+            Console.WriteLine("└──────────────────────────────────────────┘");
+        }
+
+        public void seleccion(ConsoleKey ck, Tv tele)
+        {
+            switch(ck)
+            {
+                case ConsoleKey.Enter:
+                    tele.pulsarOnOff();
+                    tele.mostrarEstado();
+                    break;
+                case ConsoleKey.Spacebar:
+                    if (tele.estadoActual())
+                    {
+                        Console.Write("\nIntroduzca un ");
+                        impVerde("canal: ");
+                        int canal = comprobarNumInt(Console.ReadLine());
+                        tele.ponerCanal(canal);
+                        tele.mostrarEstado();
+                    }
+                    break;
+                case ConsoleKey.Backspace:
+                    if (tele.estadoActual())
+                    {
+                        tele.cambiarCanalAnterior();
+                        tele.mostrarEstado();
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (tele.estadoActual())
+                    {
+                        tele.subirVolumen();
+                        tele.mostrarEstado();
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (tele.estadoActual())
+                    {
+                        tele.bajarVolumen();
+                        tele.mostrarEstado();
+                    }
+                    break;
+                case ConsoleKey.I:
+                    tele.informacionTecnica();
+                    break;
+                default:
+                    impError("\nERROR. Opción no válida.\n");
+                    break;
+            }
+        }
+        #endregion
+    }
+}
---
+++ ConsoleApp1/Tv.cs
@@ -0,0 +1,229 @@
+﻿/*
+* PRÁCTICA.............: Práctica 1.
+* NOMBRE y APELLIDOS...: Sara Blanco Muñoz
+* CURSO y GRUPO........: 2º Desarrollo de Aplicaciones Multiplataforma
+* TÍTULO de la PRÁCTICA: Diseño de clases. Herencia y polimorfismo.
+* FECHA de ENTREGA.....: 23 de Octubre de 2017
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica1
+{
+    class Tv
+    {
+        #region Atributos
+        private string marca;
+        private double pulgadas, consumo, precio;
+        private bool onOff;
+        private int canal, canalAnterior, volumen;
+        #endregion
+
+        #region Propiedades
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public double Pulgadas
+        {
+            get { return pulgadas; }
+            set
+            {
+                if (value > 0 && value < 500)
+                {
+                    pulgadas = value;
+                }
+                else
+                {
+                    Principal.impError("\nERROR. Pulgadas erróneas.\n");
+                    Principal.continuar();
+                }
+            }
+        }
+
+        public double Consumo
+        {
+            get { return consumo; }
+            set
+            {
+                if (value > 0)
+                {
+                    consumo = value;
+                }
+                else
+                {
+                    Principal.impError("\nERROR. Consumo erróneo.\n");
+                    Principal.continuar();
+                }
+            }
+        }
+
+        public double Precio
+        {
+            get { return precio; }
+            set
+            {
+                if (value > 0)
+                {
+                    precio = value;
+                }
+                else
+                {
+                    Principal.impError("\nERROR. Precio erróneo.\n");
+                    Principal.continuar();
+                }
+            }
+        }
+
+        public bool OnOff
+        {
+            get { return onOff; }
+            set { onOff = value; }
+        }
+
+        public int Canal
+        {
+            get { return canal; }
+            set
+            {
+                if (value >= 0 && value < 100)
+                    canal = value;
+                else
+                {
+                    Console.Beep();
+                    Principal.impError("\nCanal erróneo.\n");
+                }
+            }
+        }
+
+        public int CanalAnterior
+        {
+            get { return canalAnterior; }
+            set
+            {
+                if (value >= 0 && value < 100)
+                    canalAnterior = value;
+                else
+                    Principal.impError("\nCanal erróneo.\n");
+            }
+        }
+
+        public int Volumen
+        {
+            get
+            {
+                return volumen;
+            }
+            set
+            {
+                if (value > 0 && value < 101)
+                    volumen = value;
+                else
+                {
+                    Console.Beep();
+                    Principal.impError("\nLímite de volumen.\n");
+                }
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public Tv() { }
+
+        public Tv(string m, double pulg, double cons, double pr)
+        {
+            marca = m;
+            pulgadas = pulg;
+            consumo = cons;
+            precio = pr;
+            /*onOff = false;
+            canal = 0;
+            volumen = 1;*/
+        }
+        #endregion
+
+        #region Métodos
+        public void pulsarOnOff()
+        {
+            OnOff = !OnOff;
+
+            if (OnOff)
+            {
+                Canal = 1;
+                CanalAnterior = 1;
+                Volumen = 25;
+            }
+        }
+        
+        public void subirVolumen()
+        {
+            Volumen = volumen+1;
+        }
+
+        public void bajarVolumen()
+        {
+            Volumen = volumen - 1;
+        }
+
+        public void ponerCanal(int nCanal)
+        {
+            CanalAnterior = Canal;
+            Canal = nCanal;
+        }
+
+        public void cambiarCanalAnterior()
+        {
+            int aux = Canal;
+            Canal = CanalAnterior;
+            CanalAnterior = aux;
+        }
+
+        public bool estadoActual()
+        {
+            if(!OnOff)
+            {
+                for (int i = 0; i < 3; i++)
+                    Console.Beep();
+
+                Principal.impError("\nApagada.\n");
+            }
+
+            return OnOff;
+        }
+
+        public void informacionTecnica()
+        {
+            Principal.impCyan("\nMarca: ");
+            Console.WriteLine(Marca);
+            Principal.impCyan("Tamaño: ");
+            Console.WriteLine(Pulgadas+"\"");
+            Principal.impCyan("Precio: ");
+            Console.WriteLine(Precio+" Euros");
+        }
+
+        public void mostrarEstado()
+        {
+            if (OnOff)
+            {
+                Principal.impCyan("\nEncendida ");
+                Console.Write("->\t");
+                Principal.impCyan("Canal: ");
+                Console.Write(Canal);
+                Principal.impCyan("\tVolumen: ");
+                Console.WriteLine(Volumen);
+            }
+            else
+            {
+                Console.WriteLine("\nApagada.");
+            }
+        }
+
+        #endregion
+
+    }
+}
